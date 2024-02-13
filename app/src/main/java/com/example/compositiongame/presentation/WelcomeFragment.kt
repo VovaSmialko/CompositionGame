295090cf8ae97841aae7c4b6176772b1e1679d6d
vova77 <vova2000smi@gmail.com>
2024-02-13 20:37:36 +0200

    Add ViewBinding on Fragments

--- app/src/main/java/com/example/compositiongame/presentation/WelcomeFragment.kt
+++ app/src/main/java/com/example/compositiongame/presentation/WelcomeFragment.kt
@@ -6,13 +6,36 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.compositiongame.R
+import com.example.compositiongame.databinding.FragmentWelcomeBinding
+import java.lang.RuntimeException
 
-class WelcomeFragment: Fragment() {
+class WelcomeFragment : Fragment() {
+
+    private var _binding: FragmentWelcomeBinding? = null
+    private val binding: FragmentWelcomeBinding
+        get() = _binding ?: throw RuntimeException("FragmentWelcomeBinding == null")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+    ): View {
+        _binding = FragmentWelcomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.buttonUnderstand.setOnClickListener {
+
+        }
+    }
+
+    private fun launchChooseLevelFragment() {
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
