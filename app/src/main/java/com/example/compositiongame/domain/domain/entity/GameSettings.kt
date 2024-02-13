01fee13462a1d8c5ed60cf1d35f018b26f6e43f3
vova77 <vova2000smi@gmail.com>
2024-02-13 21:17:43 +0200

    Add FragmentManager

--- app/src/main/java/com/example/compositiongame/domain/domain/entity/GameSettings.kt
+++ app/src/main/java/com/example/compositiongame/domain/domain/entity/GameSettings.kt
@@ -1,8 +1,10 @@
 package com.example.compositiongame.domain.domain.entity
 
+import java.io.Serializable
+
 data class GameSettings(
     val maxSumValue: Int,
     val minCountOfRightQuestions: Int,
     val minPercentOfRightAnswers: Int,
     val gameTimeInSecond: Int
-)
+): Serializable
--- app/src/main/java/com/example/compositiongame/domain/domain/entity/Level.kt
+++ app/src/main/java/com/example/compositiongame/domain/domain/entity/Level.kt
@@ -1,5 +1,7 @@
 package com.example.compositiongame.domain.domain.entity
 
+import java.io.Serializable
+
 enum class Level {
 
     TEST, EASY, NORMAL, HARD
